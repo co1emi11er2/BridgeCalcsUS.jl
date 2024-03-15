7412857a1d60db585fdc32d41cdde9bebf450bc9
Documenter.jl <documenter@juliadocs.github.io>
2024-03-15 20:53:04 +0000

    build based on e811e4f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BridgeCalcsUS","category":"page"},{"location":"#BridgeCalcsUS","page":"Home","title":"BridgeCalcsUS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BridgeCalcsUS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BridgeCalcsUS]","category":"page"}]
+}
