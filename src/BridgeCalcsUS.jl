c3c0b43c948cb4f85e2bec610b105fc32d7ba169
co1emi11er2 <colemiller2010@gmail.com>
2024-04-13 08:52:52 -0500

    update

--- src/BridgeCalcsUS.jl
+++ src/BridgeCalcsUS.jl
@@ -1,34 +1,34 @@
 module BridgeCalcsUS
 
-export calc_Ix, calc_Iy, calc_I, calculate_Ix
+export A6_6_1_2_2_1
 
+"""
+    A6_6_1_2_2_1(γ, Δf, ΔF_n)
 
-function calc_Ix(b, h)
-    Ix = b*h^3/12
-    return Ix
+# Arguments
+
+- γ - load factor specified in Table 3.4.1-1 for the fatigue load combination.
+- Δf - force effect, live load stress range due to the passage of the fatigue 
+  load as specified in Article 3.6.1.4 (ksi)  
+- ΔF_n - nominal fatigue resistance as specified in Article 6.6.1.2.5 (ksi)
+"""
+function A6_6_1_2_2_1(γ, Δf, ΔF_n)
+    γ*Δf <= ΔF_n
 end
 
-function calc_Iy(h, b=15; expo=3, denominator=12)
-    Iy = h*b^expo/denominator
-    return Iy
-end
+"""
+    A6_9_4_1_2d1(E, A_g, K, l, r_s)
 
-function calc_I()
-    I = 5*15^3/12
-    # return I
-end
+# Arguments
 
-function area_sqare(side)
-    area = side^2
-end
-
-function area_rectangle(l, w)
-    area = l * w
-end
-
-function calculate_Ix(b, h)
-    Ix = b*h^3/12; "moment of inertia";
-    return Ix
+- E = modulus of elasticity of steel (ksi)
+- A_g - gross cross-sectional area of the member (in.2) 
+- K - effective length factor in the plane of buckling determined as specified in Article 4.6.2.5
+- l - unbraced length in the plane of buckling (in.)
+- r_s - radius of gyration about the axis normal to the plane of buckling (in.)
+"""
+function A6_9_4_1_2d1(E, A_g, K, l, r_s)
+    P_e = (π^2*E)/(K*l/r_s)*A_g
 end
 
 end
